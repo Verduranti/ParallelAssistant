2720b322f41f74434a43db728624229b29d14f61
verduranti <angelofchaos@gmail.com>
2015-02-21 23:25:07 -0800

    Initial Add

---
+++ app/src/main/java/com/intel/iot/autobackupcamera/MainActivity.java
@@ -0,0 +1,68 @@
+package com.intel.iot.autobackupcamera;
+
+import android.media.MediaExtractor;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    //private EditText field;
+    private WebView browser;
+
+    //private MediaCodecWrapper mCodecWrapper; //see example
+    private MediaExtractor mExtractor = new MediaExtractor();
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //field = (EditText)findViewById(R.id.urlField);
+        browser = (WebView)findViewById(R.id.webView);
+        browser.setWebViewClient(new MyBrowser());
+    }
+
+    public void open(View view){
+        String url = "http://192.168.42.1:8080";
+        browser.getSettings().setLoadsImagesAutomatically(true);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        browser.loadUrl(url);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
